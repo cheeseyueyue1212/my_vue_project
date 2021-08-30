1687c979047865060b9f8da0191a8d08519b7288
cheeseyueli <cheeseyueli@tencent.com>
2021-08-30 15:56:27 +0800

    first commit

---
+++ src/views/home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="project-version-wrapper">home</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, toRefs, reactive, nextTick } from 'vue'
+
+export default defineComponent({
+  name: 'SelectionAttr',
+  components: {},
+  setup() {
+    const state = reactive<{}>({})
+
+    nextTick(() => {
+      console.log('nextTick func')
+    })
+
+    return {
+      ...toRefs(state)
+    }
+  }
+})
+</script>
+
+<style lang="less" scoped></style>
